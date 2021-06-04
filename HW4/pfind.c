d7fe8fe23cbe998256766e5ebbbee0f9506e0af2
idan10000 <49813824+idan10000@users.noreply.github.com>
2021-06-05 00:48:32 +0300

    Update pfind.c

--- HW4/pfind.c
+++ HW4/pfind.c
@@ -84,9 +84,10 @@ node *dequeue() {
 
 
     pthread_mutex_lock(&queueMutex);
-    while (queue->len == 0) { //if queue is empty
+    //if queue is empty either wait or close the program
+    while (queue->len == 0) {
+        // if queue is empty and the thread is the last active thread, close program
         if (activeThreads == 1) {
-            printf("exiting\n");
             exitFlag = 1;
             pthread_cond_broadcast(&queueNotEmptyCV);
             pthread_cond_destroy(&queueNotEmptyCV);
@@ -95,19 +96,21 @@ node *dequeue() {
         }
         activeThreads--;
         if (activeThreads != 0)
-            pthread_cond_wait(&queueNotEmptyCV, &queueMutex);
-        if (exitFlag) {
+            pthread_cond_wait(&queueNotEmptyCV, &queueMutex); // wait until queue isn't empty and the signal was sent.
+
+        if (exitFlag) { // if the closing procedure has started
             pthread_mutex_unlock(&queueMutex);
             pthread_exit(NULL);
         }
         activeThreads++;
     }
 
+    // dequeue the head node
     node *temp = queue->head;
 
     queue->head = queue->head->next;
     queue->len--;
-    if(queue->head == NULL)
+    if (queue->head == NULL)
         queue->tail = NULL;
 
     temp->next = NULL;
@@ -116,6 +119,7 @@ node *dequeue() {
     return temp;
 }
 
+// create the working threads of the program
 void createThreads(pthread_t *threads, int threadAmount, char *searchTerm) {
     for (size_t i = 0; i < threadAmount; i++) {
         activeThreads++;
@@ -125,12 +129,13 @@ void createThreads(pthread_t *threads, int threadAmount, char *searchTerm) {
             exit(EXIT_FAILURE);
         }
     }
+    // send starting signal and update flag
     startFlag = 1;
-
     pthread_cond_broadcast(&startThreadsCV);
     pthread_cond_destroy(&startThreadsCV);
 }
 
+// check if dir is searchable
 char isDirSearchable(char *path) {
     return access(path, R_OK | X_OK);
 }
@@ -153,6 +158,7 @@ void *handleThreads(void *searchTerm) {
 
             // check if curEntry is a directory
             if (curEntry->d_type == DT_DIR) {
+                //create path to the new dir
                 char *newPath = malloc(PATH_MAX);
                 if (newPath == NULL) {
                     fprintf(stderr, "Failed allocating memory: %s\n", strerror(ENOMEM));
@@ -168,19 +174,17 @@ void *handleThreads(void *searchTerm) {
                     continue;
                 }
 
+                // open dir and insert it into queue
                 DIR *tempDir = opendir(newPath);
 
                 if (tempDir == NULL) {
                     fprintf(stderr, "Directory failed to open: %s\n", strerror(errno));
                     activeThreads--;
-
                     pthread_cond_signal(&queueNotEmptyCV);
                     pthread_exit(NULL);
                 }
                 enqueue(tempDir, newPath);
-            }
-                // if curEntry contains the search term
-            else {
+            } else {   // if curEntry contains the search term
                 if (strstr(curEntry->d_name, term) != NULL) {
                     char *newPath = malloc(PATH_MAX);
                     if (newPath == NULL) {
@@ -201,6 +205,7 @@ void *handleThreads(void *searchTerm) {
 
 
 int main(int argc, char *argv[]) {
+    // check if enough arguments inserted
     if (argc < 4) {
         fprintf(stderr, "Not enough arguments inserted: %s\n", strerror(EINVAL));
         exit(EXIT_FAILURE);
@@ -210,12 +215,14 @@ int main(int argc, char *argv[]) {
     char *searchTerm = argv[2];
     int threadAmount = atoi(argv[3]);
 
+    // check if root dir is searchable
     if (isDirSearchable(rootDir) == -1) {
         printf("Directory %s: Permission denied.\n", rootDir);
         fprintf(stderr, "Root directory is not searchable: %s\n", strerror(EINVAL));
         exit(EXIT_FAILURE);
     }
 
+    // create the queue and initialize it to start from root dir
     queue = malloc(sizeof(MyQueue));
     if (queue == NULL) {
         fprintf(stderr, "Failed allocating memory: %s\n", strerror(ENOMEM));
@@ -247,18 +254,22 @@ int main(int argc, char *argv[]) {
     pthread_cond_init(&startThreadsCV, NULL);
     pthread_mutex_init(&startMutex, NULL);
 
+    // Init flags
     exitFlag = 0;
     startFlag = 0;
     activeThreads = 0;
     filesFound = 0;
     pthread_t threads[threadAmount];
 
+    //start program
     createThreads(threads, threadAmount, searchTerm);
 
+    // wait for all threads to exit
     for (int i = 0; i < threadAmount; i++) {
         pthread_join(threads[i], NULL);
     }
 
+    // print result
     printf("Done searching, found %d files\n", filesFound);
     pthread_mutex_destroy(&queueMutex);
     exit(EXIT_SUCCESS);
